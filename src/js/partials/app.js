f29b24c14b2bce742c313f3a0d5e22f0e51ce0d2
Amy Kapernick <amy@aimhigherwebdesign.com>
2018-05-21 14:25:01 +0800

    changed quote to dark grey

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -9,19 +9,50 @@ import favicons from '../../img/favicons/*';
 import { Header } from './header.js';
 import { Home } from '../layouts/home.js';
 import { Footer } from './footer.js';
+import Painting from '../../img/legal-eagles.jpg';
 
 class Meta extends Component {
 	render() {
+		let name = 'David Manera | Barrister and Solicitor';
+		let description = 'David Manera is a Perth-based lawyer specialising in Criminal Law practising throughout Western Australia';
+		let slug = '';
+		let image = Painting;
+		let siteUrl = 'davidmanera.com.au';
+
 		return (
 			<Helmet>
+				<title>{name}</title>
+				<meta name="description" content={description} />
+				<link
+					rel="canonical"
+					href={siteUrl + slug}
+				/>
+
+				{/* Facebook */}
+				<meta
+					property="og:url"
+					content={siteUrl + slug}
+				/>
+
+				<meta property="og:title" content={name} />
+				<meta property="og:image" content={image} />
+				<meta property="og:description" content={description} />
+
+				{/* Twitter */}
+				<meta
+					name="twitter:url"
+					content={siteUrl + slug}
+				/>
+				<meta name="twitter:title" content={name} />
+				<meta name="twitter:description" content={description} />
+				<meta name="twitter:image" content={image} />
+
 				<link rel="shortcut icon" href={favicons['favicon.png']} />
 				<link rel="icon" type="image/png" sizes="16x16" href={favicons['favicon-16.png']} />
 				<link rel="icon" type="image/png" sizes="32x32" href={favicons['favicon-32.png']} />
 				<link rel="icon" type="image/png" sizes="96x96" href={favicons['favicon-96.png']} />
 
-				<link rel="apple-touch-icon" href={favicons['favicon-180.png']} />
 				<meta name="theme-color" content="#1C75BC" />
-				<link rel="icon" sizes="192x192" href={favicons['favicon-192.png']} />
 			</Helmet>
 		);
 	}
