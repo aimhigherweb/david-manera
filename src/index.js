aaf1cd5d32d5d8a32387e4531f146dc47f2f6096
Amy Kapernick <amy@aimhigherwebdesign.com>
2018-05-22 09:33:49 +0800

    added GTM module

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import ReactDOM from 'react-dom';
 import {render} from 'react-snapshot';
+import TagManager from 'react-gtm-module';
 
 
 //Import components
@@ -8,6 +9,13 @@ import { App } from './js/partials/app.js';
 
 //Resources
 import './scss/global.scss';
+
+const tagManagerArgs = {
+	gtmId: 'GTM-N6P4P6C'
+}
+
+TagManager.initialize(tagManagerArgs);
+
 
 class Main extends React.Component {
 	render() {
